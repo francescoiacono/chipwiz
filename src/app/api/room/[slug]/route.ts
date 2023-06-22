ed209b59456808ebf092dade3dcd5ed700cf786b
Francesco <sennozeta@gmail.com>
2023-06-22 13:27:24 +0100

    added api for adding player to room

--- src/app/api/room/[slug]/route.ts
+++ src/app/api/room/[slug]/route.ts
@@ -1,4 +1,5 @@
-import controller from '@/data/controls/rooms/controller';
+import roomController from '@/data/controls/rooms/controller';
+import playerController from '@/data/controls/players/controller';
 import { NextResponse } from 'next/server';
 
 export const GET = async (
@@ -6,11 +7,28 @@ export const GET = async (
   { params }: { params: { slug: string } }
 ): Promise<NextResponse> => {
   const { slug } = params;
-  const room = controller.getRoom(slug);
+  const room = roomController.getRoom(slug);
 
   if (!room) {
     return NextResponse.json({ error: 'Room not found' }, { status: 404 });
   }
 
   return NextResponse.json(room, { status: 200 });
+};
+
+export const POST = async (
+  req: Request,
+  { params }: { params: { slug: string } }
+): Promise<NextResponse> => {
+  const { slug } = params;
+  const body = await req.json();
+
+  const player = await playerController.getPlayer(body.playerId);
+
+  if (!player) {
+    return NextResponse.json({ error: 'Player not found' }, { status: 404 });
+  }
+
+  const updatedRoom = roomController.addPlayerToRoom(slug, player);
+  return NextResponse.json(updatedRoom, { status: 200 });
 };
--- src/data/controls/rooms/controller.ts
+++ src/data/controls/rooms/controller.ts
@@ -1,11 +1,13 @@
 import getRoom from './getRoom/getRoom';
 import addRoom from './addRoom/addRoom';
 import getAllRooms from './getAllRooms/getAllRooms';
+import addPlayerToRoom from './addPlayerToRoom/addPlayerToRoom';
 
 const controller = {
   getRoom,
   addRoom,
   getAllRooms,
+  addPlayerToRoom,
 };
 
 export default controller;
